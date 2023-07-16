cf5faebdd62acb2d6d1a740afe8f0165fd5e1892
ederson42 <139698711+ederson42@users.noreply.github.com>
2023-07-16 20:38:03 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,26 @@
+<?php
+///////////     CHAMA A BASE DO FAKE FRAMEWORK    ///////////
+require_once("src/componentes/import_framework.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+<div style="margin-left:15%">
+
+  <div class="w3-container w3-teal">
+      <h1>HOME</h1>
+  </div>
+  <h1>Pensado e criado por Éderson dos Santos da Silva</h1>
+  <h2>Email:  ederson42santossilva@gmail.com</h2>
+</div>
+<?php
+
+?>
+
+
+</body>
+</html>
