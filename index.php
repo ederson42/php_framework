2b1d3984884acd0943c15b70b268b7c85535ce45
ederson42 <139698711+ederson42@users.noreply.github.com>
2023-07-16 20:37:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+///////////     CHAMA A BASE DO FAKE FRAMEWORK    ///////////
+require_once("src/componentes/import_framework.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+<div style="margin-left:15%">
+
+<div class="w3-container w3-teal">
+    <h1>HOME</h1>
+</div>
+</div>
+
+</body>
+</html>
