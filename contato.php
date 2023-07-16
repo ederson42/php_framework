7b6b27432d8b1b1e24214940da66f589653e5eb0
ederson42 <139698711+ederson42@users.noreply.github.com>
2023-07-16 20:38:19 -0300

    Add files via upload

---
+++ contato.php
@@ -0,0 +1,22 @@
+<?php
+///////////     CHAMA A BASE DO FAKE FRAMEWORK    ///////////
+require_once("src/componentes/import_framework.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONTATO</title>
+</head>
+<body>
+  <div style="margin-left:15%">
+
+    <div class="w3-container w3-teal">
+        <h1>CONTATO</h1>
+    </div>
+    <h1>Pensado e criado por Éderson dos Santos da Silva</h1>
+    <h2>Email:  ederson42santossilva@gmail.com</h2>
+    <h2>Linkedin: https://www.linkedin.com/in/ederson-dos-santos-da-silva-264b81274 </h2>
+  </div>
+</body>
+</html>
